93f0116f8e6a69f1d60a66f0e90a26a823a1a311
Dmitry Hmel <dmitry.hmel@gmail.com>
2015-03-18 18:03:08 +0200

    add StatisticActivity

---
+++ app/src/main/java/ua/com/sober/timetracks/StatisticActivity.java
@@ -0,0 +1,10 @@
+package ua.com.sober.timetracks;
+
+import android.support.v7.app.ActionBarActivity;
+
+/**
+ * Created by dmitry.hmel on 18.03.2015.
+ */
+public class StatisticActivity extends ActionBarActivity {
+
+}
