36771f29a0685eb5f9bb2314c589c7155f84f4bf
Dmitry <dmitry.hmel@gmail.com>
2015-04-02 16:46:00 +0300

    only one task run!

--- app/src/main/java/ua/com/sober/timetracks/util/TaskTrack.java
+++ app/src/main/java/ua/com/sober/timetracks/util/TaskTrack.java
@@ -9,7 +9,8 @@ import android.util.Log;
 
 import ua.com.sober.timetracks.provider.ContractClass;
 
-import static ua.com.sober.timetracks.provider.ContractClass.TaskTracks.CONTENT_URI;
+import static ua.com.sober.timetracks.provider.ContractClass.TaskTracks;
+import static ua.com.sober.timetracks.provider.ContractClass.Tasks;
 
 /**
  * Created by dmitry.hmel on 13.03.2015.
@@ -23,6 +24,8 @@ public class TaskTrack {
     private long trackStopTime;
     private long totalTime;
     private Uri trackUri;
+    private Uri taskUri;
+    private Cursor cursor;
 
     public TaskTrack(Context context, long taskID, long status, long totalTime) {
         this.context = context;
@@ -37,9 +40,9 @@ public class TaskTrack {
         ContentValues cv = new ContentValues();
         cv.put(ContractClass.TaskTracks.COLUMN_NAME_TASK_ID, taskID);
         cv.put(ContractClass.TaskTracks.COLUMN_NAME_START_TIME, trackStartTime);
-        trackUri = context.getContentResolver().insert(CONTENT_URI, cv);
+        trackUri = context.getContentResolver().insert(TaskTracks.CONTENT_URI, cv);
         trackID = ContentUris.parseId(trackUri);
-        setStatus(trackID);
+        setStatus(taskID, trackID);
         Log.w("SQLite", "startTrack, result Uri : " + trackUri.toString());
     }
 
@@ -47,39 +50,57 @@ public class TaskTrack {
         long trackID;
         long taskID;
         long stopTime;
-        Cursor cursor = context.getContentResolver().query(CONTENT_URI, null, null, null, null);
-        cursor.moveToFirst();
-        trackID = cursor.getLong(cursor.getColumnIndex(ContractClass.TaskTracks._ID));
-        taskID = cursor.getLong(cursor.getColumnIndex(ContractClass.TaskTracks.COLUMN_NAME_TASK_ID));
-        stopTime = cursor.getLong(cursor.getColumnIndex(ContractClass.TaskTracks.COLUMN_NAME_STOP_TIME));
-        cursor.close();
-        if(stopTime == 0) {
-            
-        }
-        Log.w("SQLite","trackID: " + trackID + ", taskID: " + taskID + ", stopTime: " + stopTime);
+        long totalTime;
+        cursor = context.getContentResolver().query(TaskTracks.CONTENT_URI, null, null, null, null);
+        if ((cursor.moveToFirst()) || cursor.getCount() != 0) {
+            trackID = cursor.getLong(cursor.getColumnIndex(ContractClass.TaskTracks._ID));
+            taskID = cursor.getLong(cursor.getColumnIndex(ContractClass.TaskTracks.COLUMN_NAME_TASK_ID));
+            stopTime = cursor.getLong(cursor.getColumnIndex(ContractClass.TaskTracks.COLUMN_NAME_STOP_TIME));
+            cursor.close();
+            if (stopTime == 0) {
+                trackStopTime = System.currentTimeMillis();
+                trackUri = ContentUris.withAppendedId(TaskTracks.CONTENT_URI, trackID);
+                ContentValues cv = new ContentValues();
+                cv.put(ContractClass.TaskTracks.COLUMN_NAME_STOP_TIME, trackStopTime);
+                context.getContentResolver().update(trackUri, cv, null, null);
 
+                taskUri = ContentUris.withAppendedId(Tasks.CONTENT_URI, taskID);
+                ContentValues cvTask = new ContentValues();
+                cvTask.put(Tasks.COLUMN_NAME_STATUS, status);
+                cursor = context.getContentResolver().query(taskUri, null, null, null, null);
+                cursor.moveToFirst();
+                totalTime = cursor.getLong(cursor.getColumnIndex(Tasks.COLUMN_NAME_TOTAL_TIME));
+                cursor.close();
+                totalTime = totalTime + trackStopTime;
+                cvTask.put(Tasks.COLUMN_NAME_TOTAL_TIME, totalTime);
+                context.getContentResolver().update(taskUri, cvTask, null, null);
+
+                Log.w("SQLite", "stopTrack, result Uri : " + trackUri.toString());
+            }
+            Log.w("SQLite", "trackID: " + trackID + ", taskID: " + taskID + ", stopTime: " + stopTime);
+        } else cursor.close();
     }
 
     public void stopTrack() {
         trackStopTime = System.currentTimeMillis();
-        trackUri = ContentUris.withAppendedId(CONTENT_URI, status);
+        trackUri = ContentUris.withAppendedId(TaskTracks.CONTENT_URI, status);
         ContentValues cv = new ContentValues();
         cv.put(ContractClass.TaskTracks.COLUMN_NAME_STOP_TIME, trackStopTime);
         context.getContentResolver().update(trackUri, cv, null, null);
-        setStatus(0);
+        setStatus(taskID, 0);
         Log.w("SQLite", "stopTrack, result Uri : " + trackUri.toString());
     }
 
-    private void setStatus(long status) {
-        Uri taskUri = ContentUris.withAppendedId(ContractClass.Tasks.CONTENT_URI, taskID);
+    private void setStatus(long taskID, long status) {
+        Uri taskUri = ContentUris.withAppendedId(Tasks.CONTENT_URI, taskID);
         ContentValues cv = new ContentValues();
-        cv.put(ContractClass.Tasks.COLUMN_NAME_STATUS, status);
+        cv.put(Tasks.COLUMN_NAME_STATUS, status);
         if (status == 0) {
             totalTime = totalTime + trackStopTime;
-            cv.put(ContractClass.Tasks.COLUMN_NAME_TOTAL_TIME, totalTime);
+            cv.put(Tasks.COLUMN_NAME_TOTAL_TIME, totalTime);
         } else {
             totalTime = totalTime - trackStartTime;
-            cv.put(ContractClass.Tasks.COLUMN_NAME_TOTAL_TIME, totalTime);
+            cv.put(Tasks.COLUMN_NAME_TOTAL_TIME, totalTime);
         }
         context.getContentResolver().update(taskUri, cv, null, null);
     }
