54958a86de5447ee0560c045004cea6a1112c9d4
Dmitry Hmel <dmitry.hmel@gmail.com>
2016-03-18 00:49:10 +0200

    create class StatisticsCollector

---
+++ app/src/main/java/ua/com/sober/timetracks/util/StatisticsCollector.java
@@ -0,0 +1,14 @@
+package ua.com.sober.timetracks.util;
+
+import java.util.Map;
+
+/**
+ * Created by Dmitry on 17.03.2016.
+ */
+public class StatisticsCollector {
+    private Map<Integer, Integer> dayStatistics;
+    private Map<Integer, Integer> weekStatistics;
+    private Map<Integer, Integer> monthStatistics;
+    private Map<Integer, Integer> yearStatistics;
+    private Map<Integer, Integer> allTimeStatistics;
+}
